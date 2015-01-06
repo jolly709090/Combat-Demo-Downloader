e04b01af9b84a3122ace65cc0f2fa4d2d963f19d
xaduha <xaduha@gmail.com>
2015-01-06 09:40:30 +0300

    Added SHA256 check of manifest.gz

--- main.ps1
+++ main.ps1
@@ -8,29 +8,35 @@ $ProgressPreference = "SilentlyContinue"
 
 Invoke-WebRequest -Uri $uri -UseBasicParsing -OutFile manifest.gz
 
-[xml]$manifest = ConvertFrom-Gzip -Path manifest.gz
+$temp = Get-FileHash -Path manifest.gz -Algorithm SHA256
 
-function alreadyDownloaded($destPath, $hash) {
-    $result = Get-FileHash -Path $destPath -Algorithm SHA1 -ErrorAction SilentlyContinue
-    $result.Hash -eq $hash
-}
+if ($temp.Hash -eq 'E42A3DA31255DFFA7FEB4D46B83AD4A9AD897FAC00F3228031819F94A5185F6B') {
+    [xml]$manifest = ConvertFrom-Gzip -Path manifest.gz
 
-$nodes = $manifest.SelectNodes('/Manifest/File')
-
-foreach ($node in $nodes) {
-
-    $destPath = $dir+$node.Path
-
-    if (alreadyDownloaded -destPath $destPath -hash $node.Hash) {
-        "Skipping    " + $destPath
-    } else {
-        $destFolder = Split-Path -Path $destPath
-        $silent = New-Item -Path $destFolder -ItemType Directory -ErrorAction SilentlyContinue
-        "Downloading " + $destPath
-        Invoke-WebRequest -Uri $node.Download -UseBasicParsing -OutFile $destPath
+    function alreadyDownloaded($destPath, $hash) {
+        $result = Get-FileHash -Path $destPath -Algorithm SHA1 -ErrorAction SilentlyContinue
+        $result.Hash -eq $hash
     }
 
-}
+    $nodes = $manifest.SelectNodes('/Manifest/File')
 
-$VS2012 = 'http://download.microsoft.com/download/1/6/B/16B06F60-3B20-4FF2-B699-5E9B7962F9AE/VSU_4/vcredist_x86.exe'
-Invoke-WebRequest -Uri $VS2012 -UseBasicParsing -OutFile $dir+'vcredist_x86.exe'
+    foreach ($node in $nodes) {
+
+        $destPath = $dir+$node.Path
+
+        if (alreadyDownloaded -destPath $destPath -hash $node.Hash) {
+            "Skipping    " + $destPath
+        } else {
+            $destFolder = Split-Path -Path $destPath
+            $silent = New-Item -Path $destFolder -ItemType Directory -ErrorAction SilentlyContinue
+            "Downloading " + $destPath
+            Invoke-WebRequest -Uri $node.Download -UseBasicParsing -OutFile $destPath
+        }
+
+    }
+
+    $VS2012 = 'http://download.microsoft.com/download/1/6/B/16B06F60-3B20-4FF2-B699-5E9B7962F9AE/VSU_4/vcredist_x86.exe'
+    Invoke-WebRequest -Uri $VS2012 -UseBasicParsing -OutFile $dir+'vcredist_x86.exe'
+} else {
+    'Hash mismatch, check for update at https://github.com/xaduha/Combat-Demo-Downloader'
+}
